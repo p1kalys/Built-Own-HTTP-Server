0c1532e1f3ed768c6db17202efa1d7eed88eb472
p1kalys <pavanemani14@gmail.com>
2024-04-19 23:59:23 +0530

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -1,46 +1,64 @@
-const net = require("net");
+const net = require('net');
+const CRLF = '\r\n\r\n';
+const server = net.createServer((socket) => {
+  socket.on('close', () => {
+    console.log('[Socket] closed');
+    socket.end();
+    server.close();
+  });
+  socket.on('data', (data) => {
+    const parsedData = parseData(data);
+    console.log('[Socket] data received', parsedData);
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+    socket.write(routeRequest(parsedData.path));
 
-const CRLF = "\r\n\r\n";
+    socket.end();
+  });
+});
 
+/**
+ * @param path {String}
+ */
+function routeRequest(path) {
+  if (path === '/') {
+    return `HTTP/1.1 200 OK${CRLF}`;
+  }
+
+  if (path.startsWith('/echo/')) {
+    const str = path.substring(6); // starting index after `/echo/`
+    return `HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+Content-Length: ${str.length}\r
+\r
+${str}\r
+`;
+  }
+
+  return `HTTP/1.1 404 Not Found${CRLF}`;
+}
+
+/**
+ * @param dataBuf {Buffer}
+ */
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
 function parseData(dataBuf) {
-  const data = dataBuf.toString().split("\r\n");
-  const [method, path, version] = data[0].split(" ");
+  const data = dataBuf.toString();
+  const lines = data.split('\r\n');
+  const [method, path, version] = lines[0].split(' ');
   return {
     path,
     method,
     version,
   };
 }
-
-function routeRequest(path) {
-  if (path === "/") {
-    return `HTTP/1.1 200 OK${CRLF}`;
-  }
-
-  if (path.startsWith("/echo/")) {
-    const str = path.split("/echo/")[1]; // starting index after `/echo/`
-    return `HTTP/1.1 200 OK \r\n
-  Content-Type: text/plain\r\n
-  Content-Length: ${str.length}${CRLF}
-  ${str}
-  `;
-  }
-
-  return `HTTP/1.1 404 Not Found${CRLF}`;
-}
-
-const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
-    const parsedData = parseData(data);
-    socket.write(routeRequest(parsedData.path));
-  });
-  socket.on("close", ()=> {
-    socket.end();
-    socket.close();
-  })
-});
-//
-server.listen(4221, "localhost");
+server.listen(4221, 'localhost');
